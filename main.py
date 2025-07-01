985978c957ae8f185fc8bc53643e4b5540955c72
bhargav1213 <149645505+bhargav1213@users.noreply.github.com>
2025-07-01 16:09:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,15 +15,11 @@ import gdown
 
 @st.cache_resource
 def load_remote_model():
-    url = 'https://drive.google.com/file/d/1cwXTjJ8KvTlrdqxT5k3tNwf3_HIeSKMU/view?usp=sharing'  
+    url = 'https://drive.google.com/file/d1cwXTjJ8KvTlrdqxT5k3tNwf3_HIeSKMU/view?usp=sharing'  
     output = 'pneumonia_model.h5'
     if not os.path.exists(output):
         gdown.download(url, output, quiet=False)
     return load_model(output)
-
-
-
-
 # Load GoMapsPro API key from secrets
 try:
     GOOGLE_API_KEY = st.secrets["general"]["GOOGLE_API_KEY"]
